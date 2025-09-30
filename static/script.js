da1344d5145612311d3fc0f0f00feb81d1fcef2e
Lovro <lovroperaic@gmail.com>
2025-09-30 11:56:26 +0200

    click on overlay exits menu and disable scrolling when menu open

--- static/script.js
+++ static/script.js
@@ -4,6 +4,7 @@ const mediaQuery = window.matchMedia("(min-width: 56rem)");
 const openMenuBtn = document.querySelector("#menu-button");
 
 let menuToggled = false;
+let scrollY = 0;
 
 document.addEventListener("DOMContentLoaded", init);
 
@@ -12,6 +13,11 @@ function closeMenu() {
     menu.classList.remove("open");
     menuToggled = false;
     openMenuBtn.focus();
+
+    document.body.style.position = "";
+    document.body.style.top = "";
+    document.body.style.width = "";
+    window.scrollTo(0, scrollY);
 }
 
 function init() {
@@ -21,17 +27,10 @@ function init() {
         if (e.key === "Escape" && menuToggled) closeMenu();
     });
     document.addEventListener("click", e => {
-        if (!menu.contains(e.target)
-            && !closeMenuBtn.contains(e.target)
-            && !openMenuBtn.contains(e.target)
-            && menuToggled
-        ) {
-            closeMenu();
-        }
+       if (e.target.classList.contains("overlay")) closeMenu();
     });
     window.addEventListener("resize", () => {
         if (mediaQuery.matches && menuToggled) closeMenu();
-        console.log(document.activeElement)
     });
 }
 
@@ -39,4 +38,9 @@ function openMenu() {
     menu.classList.add("open");
     menuToggled = true;
     closeMenuBtn.focus();
+
+    scrollY = window.scrollY;
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${scrollY}px`;
+    document.body.style.width = "100%";
 }
